784ae1f32cd8854fd52094a5151897425f8ffbb6
AdamStilwell <126099901+AdamStilwell@users.noreply.github.com>
2023-09-07 17:07:29 -0400

    Add files via upload
    Made this a spring project.

---
+++ whats4dinner/src/main/java/com/StilwellProds/whats4dinner/Whats4dinnerApplication.java
@@ -0,0 +1,13 @@
+package com.StilwellProds.whats4dinner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Whats4dinnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Whats4dinnerApplication.class, args);
+	}
+
+}
---
+++ whats4dinner/src/test/java/com/StilwellProds/whats4dinner/Whats4dinnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StilwellProds.whats4dinner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Whats4dinnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
